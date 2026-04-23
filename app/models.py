d38318aa28a21d392f408b7b8bc0cac4319d1438
Franco Carames <caramesfranco@gmail.com>
2026-04-23 20:58:57 -0300

    updated user model

--- app/models.py
+++ app/models.py
@@ -1,15 +1,20 @@
 from app import db
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'ias_users'
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    birthdate = db.Column(db.Date, nullable=True)
     
     def to_dict(self):
         return {
             "id": self.id,
-            "name": self.name,
-            "email": self.email
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "birthdate": self.birthdate.isoformat() if self.birthdate else None
         }
